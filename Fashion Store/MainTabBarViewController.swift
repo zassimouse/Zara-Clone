ed71639d654ad99004f08303e5bbd554d0dade4d
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-07-27 16:45:51 +0300

    Add new icons

--- Fashion Store/MainTabBarViewController.swift
+++ Fashion Store/MainTabBarViewController.swift
@@ -26,36 +26,39 @@ class MainTabBarViewController: UITabBarController {
         view.backgroundColor = .systemBackground
         tabBar.tintColor = .label
         
+        let lineView = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.size.width, height: 1))
+        lineView.backgroundColor = UIColor.label
+        tabBar.addSubview(lineView)
+        
         tabBar.barTintColor = .systemBackground
         
         tabBar.items?[1].setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Arial", size: 20) ?? .thinFont, NSAttributedString.Key.backgroundColor: UIColor(.red)], for: .normal)
         
    
         let vc1 = UINavigationController(rootViewController: HomeViewController())
-        let vc2 = UINavigationController(rootViewController: SearchViewController())
-        let vc3 = UINavigationController(rootViewController: AccountViewController())
+        let vc2 = UINavigationController(rootViewController: HomeViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: BasketViewController())
-        
-//        vc1.tabBarItem.image = UIImage(systemName: "house")
-//        vc2.tabBarItem.image = UIImage(systemName: "list.bullet")
-//        vc3.tabBarItem.image = UIImage(systemName: "person.fill")
-//        vc4.tabBarItem.image = UIImage(systemName: "basket.fill")
-        
+        let vc5 = UINavigationController(rootViewController: AccountViewController())
 
-        vc1.title = "HOME"
-        vc2.title = "MENU"
-        vc3.title = "ACCOUNT"
-        vc4.title = "BAG"
         
-        vc1.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
-        vc2.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
-        vc3.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
-        vc4.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
+        vc1.tabBarItem.image = UIImage(named: "homeIcon")
+        vc2.tabBarItem.image = UIImage(named: "searchIcon")
+        vc4.tabBarItem.image = UIImage(named: "basketIcon")
+        vc5.tabBarItem.image = UIImage(named: "accountIcon")
+        
+        vc3.title = "MENU"
+
+//        vc1.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
+//        vc2.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
+        vc3.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -20)
+//        vc4.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
+//        vc5.tabBarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
         
         tabBar.barTintColor = .systemBackground
         
         UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.thinFont, NSAttributedString.Key.foregroundColor: UIColor.label], for: .normal)
         
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: true)
     }
 }
